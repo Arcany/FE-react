2eb3ad13ab3629a4e993494b613c6a3ab4c3f862
Arcany <makserik@hotmail.com>
2021-03-24 03:36:11 +0200

    Fix product counter, add styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,40 @@ import "./App.css";
 import { React, useState } from "react";
 import { ProductForm } from "./components/ProductForm";
 import { ProductGrid } from "./components/ProductGrid";
+import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   const [productList, setProductList] = useState([]);
+  const [cart, setCart] = useState([]);
   const updateProductList = (eachProduct) => {
     setProductList([...productList, eachProduct]);
   };
+  const updateCart = (eachProduct) => {
+    setCart([...cart, eachProduct]);
+  };
   return (
     <>
-      <div>
-        <h3>Products in cart: {productList.length}</h3>
-        <ProductForm updateProductList={updateProductList} />
-        <ProductGrid productList={productList} />
+      <div className="App">
+        <Container fluid>
+          <Row>
+            <Col>
+              <h1>Add product form</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3>Products in cart: {cart.length.toString()}</h3>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <ProductForm updateProductList={updateProductList} />
+            </Col>
+          </Row>
+          <Row>
+            <ProductGrid productList={productList} updateCart={updateCart} />
+          </Row>
+        </Container>
       </div>
     </>
   );
--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,33 +1,36 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
 import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
+import "./ProductGrid.css"
 export const ProductGrid = (props) => {
-  const { productList } = props;
+  const { productList, updateCart } = props;
   return (
-    <div>
-      <CardDeck>
-        <RenderProductList productList={productList} />
-      </CardDeck>
-    </div>
+    <RenderProductList productList={productList} updateCart={updateCart} />
   );
 };
+
 const RenderProductList = (props) => {
-  const { productList } = props;
+  const { productList, updateCart } = props;
   return Object.keys(productList).map((i, o) => {
     const { image, name, price } = productList[i];
+    const handleAddtoCart = (e) => {
+      return updateCart(productList[i]);
+    };
     if (image === "") return null;
     return (
-      <Card>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{price}</Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <Button>Add to cart</Button>
-        </Card.Footer>
-      </Card>
+      <Col>
+        <Card key={i.toString()}>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>{price}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Button onClick={handleAddtoCart}>Add to cart</Button>
+          </Card.Footer>
+        </Card>
+      </Col>
     );
   });
 };
